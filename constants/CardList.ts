4acb346b788323a3e31ce174fa522807029edab2
Z <zilvistam@gmail.com>
2024-10-13 10:01:47 +0100

    temp cardlist

---
+++ constants/CardList.ts
@@ -0,0 +1,22 @@
+export const cardList: Card[] = [
+  {
+    name: "A Display of My Dark Power",
+    multiverse_id: 212578,
+    img: "./../../assets/images/cardsTemp/a-display-of-my-dark-power.jpg",
+  },
+  {
+    name: "All in Good Time",
+    multiverse_id: 212648,
+    img: "./../../assets/images/cardsTemp/all-in-good-time.bmp",
+  },
+  {
+    name: "All Shall Smolder in My Wake",
+    multiverse_id: 212617,
+    img: "./../../assets/images/cardsTemp/all-shall-smolder-in-my-wake.bmp",
+  },
+  {
+    name: "Approach My Molten Realm",
+    multiverse_id: 212591,
+    img: "./../../assets/images/cardsTemp/approach-my-molten-realm.bmp",
+  },
+];
