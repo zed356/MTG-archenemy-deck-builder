14fb240a41ec2c8c7177f1837a3adff809e844d1
Z <zilvistam@gmail.com>
2024-10-13 19:12:35 +0100

    feat: can press on card to make it bigger

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,5 @@
-import { View, StyleSheet, ActivityIndicator, TouchableWithoutFeedback } from "react-native";
-import { ICard } from "@/types/types";
+import { View, StyleSheet, ActivityIndicator, Pressable, Platform } from "react-native";
 import { Image } from "expo-image";
-import FastImage from "react-native-fast-image";
 import { useState } from "react";
 import { ScryfallCard } from "@scryfall/api-types";
 
@@ -11,44 +9,62 @@ interface InputProps {
 
 const Card: React.FC<InputProps> = ({ card }) => {
   const [loading, setLoading] = useState(true);
+  const [isSelected, setIsSelected] = useState(false);
 
   const handleImageLoadEnd = () => {
     setLoading(false);
   };
 
   const styles = StyleSheet.create({
-    container: {
-      opacity: 1, // 20% less opacity for unselected cards
-      paddingTop: 0,
-    },
     card: {
-      width: 160, // 50% larger if selected
+      width: 160,
       height: 230,
+      zIndex: 1,
       marginBottom: 10,
       borderRadius: 11,
     },
+    containerIsSelected: { position: "absolute", zIndex: 1000, top: "25%", left: "25%" },
+    cardIsSelected: {
+      // position: "absolute",
+      // top: "50%",
+      // left: "50%",
+      transform: [
+        { translateX: -60 }, // Adjust based on half of the width
+        { translateY: -90 }, // Adjust based on half of the height
+      ],
+      width: 300, // 50% larger
+      height: 400,
+      // zIndex: 1000, // High zIndex
+      // backgroundColor: "rgba(128, 0, 128, 0.5)", // Optional for visibility
+    },
   });
 
+  const cardStyle = isSelected ? [styles.card, styles.cardIsSelected] : styles.card;
+
+  // Wrap the Image component in a View with position: 'relative'
   return (
-    <TouchableWithoutFeedback>
-      <View style={styles.container}>
-        {loading && <ActivityIndicator size="large" color="#FFD700" />}
+    <Pressable
+      onPress={() => setIsSelected((oldState) => !oldState)}
+      style={isSelected && styles.containerIsSelected}
+    >
+      {loading && <ActivityIndicator size="large" color="#FFD700" />}
+      <View>
         <Image
-          style={styles.card}
+          style={cardStyle}
           source={
             typeof card.image_uris!.normal === "string"
               ? { uri: card.image_uris!.normal }
               : card.image_uris!.normal
           }
-          contentFit="cover"
-          transition={1000}
+          contentFit="contain"
+          // transition={500}
           onError={() => {
-            console.log("nx bl" + card.image_uris!.normal);
+            console.log("Error loading image: " + card.image_uris!.normal);
           }}
           onLoadEnd={handleImageLoadEnd}
         />
       </View>
-    </TouchableWithoutFeedback>
+    </Pressable>
   );
 };
 
