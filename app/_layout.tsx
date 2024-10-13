f66f7c787a348e758febe37d777e3c6c8f0ee53b
Z <zilvistam@gmail.com>
2024-10-13 12:08:56 +0100

    refactor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,8 +24,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
