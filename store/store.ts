d5e8b4f7e88352df8c864bb7e44b309b599d9fb9
Z <zilvistam@gmail.com>
2024-10-19 16:48:00 +0100

    added global state for saveddecks

--- store/store.ts
+++ store/store.ts
@@ -8,6 +8,14 @@ interface NewDeckState {
   clearNewDeck: () => void;
 }
 
+interface SavedDeckState {
+  savedDecksInState: ScryfallCard.Scheme[][];
+  saveDeckToState: (deck: ScryfallCard.Scheme[]) => void;
+  removeDeckFromState: (deck: ScryfallCard.Scheme[]) => void;
+  loadDecksFromStorageIntoState: (decks: ScryfallCard.Scheme[][]) => void;
+  clearDecks: () => void;
+}
+
 export const useNewDeckStore = create<NewDeckState>((set) => ({
   cardsInNewDeck: [],
   addCardToNewDeck: (card) => set((state) => ({ cardsInNewDeck: [...state.cardsInNewDeck, card] })),
@@ -16,4 +24,14 @@ export const useNewDeckStore = create<NewDeckState>((set) => ({
       cardsInNewDeck: state.cardsInNewDeck.filter((el) => el.name !== card.name),
     })),
   clearNewDeck: () => set({ cardsInNewDeck: [] }),
+}));
+
+export const useSavedDeckStore = create<SavedDeckState>((set) => ({
+  savedDecksInState: [],
+  loadDecksFromStorageIntoState: (decks) => set({ savedDecksInState: decks }),
+  saveDeckToState: (deck) =>
+    set((state) => ({ savedDecksInState: [...state.savedDecksInState, deck] })),
+  removeDeckFromState: (deck) =>
+    set((state) => ({ savedDecksInState: state.savedDecksInState.filter((el) => el !== deck) })),
+  clearDecks: () => set({ savedDecksInState: [] }),
 }));
