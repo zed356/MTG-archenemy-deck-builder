c0b884e6a28ecf8a359b9e35f61009a72887ca35
Z <zilvistam@gmail.com>
2024-10-19 12:55:07 +0100

    background gradient

---
+++ components/GradientBackground.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, View } from "react-native";
+
+interface InputProps {
+  children?: React.ReactNode;
+  reversed?: boolean;
+}
+
+const GradientBackground: React.FC<InputProps> = ({ children, reversed = false }) => {
+  const colors = reversed ? ["#000000", "#2E0054"] : ["#2E0054", "#000000"];
+
+  return (
+    <LinearGradient colors={colors} style={styles.gradient}>
+      {children}
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1, // Fill the entire space
+  },
+});
+
+export default GradientBackground;
