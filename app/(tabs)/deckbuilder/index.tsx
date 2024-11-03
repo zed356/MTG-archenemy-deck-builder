080a52dd3a3dc52e5a2efe793382d3b951eaf16d
z <zilvistam@gmail.com>
2024-11-03 14:26:15 +0000

    added safeareaview with margin to sit above navigation tabs

--- app/(tabs)/deckbuilder/index.tsx
+++ app/(tabs)/deckbuilder/index.tsx
@@ -1,10 +1,13 @@
 import GradientBackground from "@/components/style-elements/GradientBackground";
 import DeckBuilder from "@/components/navigation/DeckBuilder";
+import { SafeAreaView } from "react-native";
 
 const DeckBuilderScreen = () => {
   return (
     <GradientBackground>
-      <DeckBuilder />
+      <SafeAreaView style={{ flex: 1, marginBottom: 30 }}>
+        <DeckBuilder />
+      </SafeAreaView>
     </GradientBackground>
   );
 };
