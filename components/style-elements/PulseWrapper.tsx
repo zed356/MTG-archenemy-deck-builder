4cf2a658660ce6157ac1e805e57df124e4c01c96
z <zilvistam@gmail.com>
2024-11-03 09:30:30 +0000

    styled element 'pulse' on change

---
+++ components/style-elements/PulseWrapper.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
+
+interface PulseWrapperProps {
+  children: React.ReactNode;
+  newDeckCardCount: number;
+}
+
+const PulseWrapper: React.FC<PulseWrapperProps> = ({ children, newDeckCardCount }) => {
+  const scale = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  useEffect(() => {
+    if (newDeckCardCount > 0) {
+      scale.value = withTiming(1.1, {
+        duration: 100,
+        easing: Easing.out(Easing.exp),
+      });
+
+      // Reset scale back to 1 after the pulse
+      const timeout = setTimeout(() => {
+        scale.value = withTiming(1, { duration: 100 });
+      }, 100);
+
+      return () => clearTimeout(timeout); // Cleanup on unmount
+    }
+  }, [newDeckCardCount, scale]);
+
+  return (
+    <View>
+      <Animated.View style={animatedStyle}>{children}</Animated.View>
+    </View>
+  );
+};
+
+export default PulseWrapper;
