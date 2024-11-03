4766dd6154b458b54391524424a0628d35141971
z <zilvistam@gmail.com>
2024-11-03 14:27:21 +0000

    changed grey to slightly lighter

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,8 +2,9 @@ export const defaultColors = {
   border: "#FFC107",
   green: "#42b883",
   gold: "#FFC107",
+  accentGold: "#FFCA28",
   red: "#FF5722",
-  grey: "#9E9E9E",
+  grey: "#B0B0B0",
   purple: "#590080",
   mainGradient: ["#2E0054", "#000000"],
 };
