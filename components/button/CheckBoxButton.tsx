4d6ea8b034c56098648d9a064946f4da11921b8c
z <zilvistam@gmail.com>
2024-11-03 14:28:36 +0000

    moved 'checked' state logic to parent component

--- components/button/CheckBoxButton.tsx
+++ components/button/CheckBoxButton.tsx
@@ -1,18 +1,21 @@
 import { defaultColors } from "@/constants/Colors";
 import { globalStyles } from "@/constants/styles";
 import { FontAwesome } from "@expo/vector-icons";
-import { useState } from "react";
 import { Pressable, Text, View, StyleSheet } from "react-native";
 
 interface CheckBoxButtonInputs {
   text: string;
-  onCheckChange: (checked: boolean) => void;
+  checked: boolean;
+  onCheckChange: () => void;
   inactiveColor?: string;
 }
 
-const CheckBoxButton: React.FC<CheckBoxButtonInputs> = ({ text, onCheckChange, inactiveColor }) => {
-  const [checked, setChecked] = useState<boolean>(true);
-
+const CheckBoxButton: React.FC<CheckBoxButtonInputs> = ({
+  text,
+  onCheckChange,
+  inactiveColor,
+  checked,
+}) => {
   const icon = checked ? "check-square" : "square";
 
   const styles = StyleSheet.create({
@@ -30,15 +33,16 @@ const CheckBoxButton: React.FC<CheckBoxButtonInputs> = ({ text, onCheckChange, i
   return (
     <Pressable
       onPress={() => {
-        setChecked((prev) => {
-          onCheckChange(!prev);
-          return !prev;
-        });
+        onCheckChange();
       }}
     >
       <View style={styles.container}>
         <Text style={[globalStyles.text, styles.text]}>{text}</Text>
-        <FontAwesome name={icon} size={24} color={checked ? defaultColors.gold : inactiveColor} />
+        <FontAwesome
+          name={icon}
+          size={24}
+          color={checked ? defaultColors.gold : inactiveColor}
+        />
       </View>
     </Pressable>
   );
