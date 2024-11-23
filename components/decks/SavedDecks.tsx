40a0942b035c20e5c3af39f6902d9d27c7b1b65a
z <zilvistam@gmail.com>
2024-11-23 15:33:29 +0000

    refactored to use FlatList instead of map, other minor syntax changes

--- components/decks/SavedDecks.tsx
+++ components/decks/SavedDecks.tsx
@@ -1,14 +1,21 @@
 import { defaultColors } from "@/constants/Colors";
 import { globalStyles } from "@/constants/styles";
-import { SAVED_DECKS_PER_PAGE } from "@/constants/values";
 import { removeDeckFromStorage, updateDeckInStorage } from "@/helpers/savedDeckManager";
 import { SavedDeck, useSavedDeckStore } from "@/store/store";
-import { FontAwesome6 } from "@expo/vector-icons";
+import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import AnimatedIcon from "../button/AnimatedIcon";
+import { useCallback, useEffect, useReducer, useState } from "react";
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import CustomButton from "../button/CustomButton";
 import ConfirmationModal from "../modals/specific-modals/ConfirmationModal";
 import SavedDeckModal from "../modals/specific-modals/SavedDeckModal";
@@ -17,46 +24,63 @@ import Spacer from "../style-elements/Spacer";
 import TabsIcon from "../style-elements/TabsIcon";
 
 interface SavedDecksProps {
-  canDeleteDeck?: boolean;
-  canClearDeckList?: boolean;
-  onDeckSelectedForPlay?: (deck: SavedDeck) => void;
+  onDeckSelectedForPlay: (deck: SavedDeck) => void;
 }
 
-const SavedDecks: React.FC<SavedDecksProps> = ({
-  canDeleteDeck = true,
-  canClearDeckList = true,
-  onDeckSelectedForPlay,
-}) => {
+// Define the types for state and actions
+type State = {
+  modalVisible: boolean;
+  selectedDeck: SavedDeck | null;
+};
+
+type Action = { type: "OPEN_MODAL"; payload: SavedDeck } | { type: "CLOSE_MODAL" };
+
+// Reducer function
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "OPEN_MODAL":
+      return { ...state, modalVisible: true, selectedDeck: action.payload };
+    case "CLOSE_MODAL":
+      return { ...state, modalVisible: false, selectedDeck: null };
+    default:
+      return state;
+  }
+};
+
+const SavedDecks: React.FC<SavedDecksProps> = ({ onDeckSelectedForPlay }) => {
   const { savedDecksInState, removeDeckFromState, updateDeckInState, clearDecks } =
     useSavedDeckStore();
-  const [deckListModalIsVisible, setDeckListModalIsVisible] = useState(false);
   const [clearDecksConfirmationModalIsVisible, setClearDecksConfirmationModalIsVisible] =
     useState(false);
-  const [selectedDeck, setSelectedDeck] = useState<SavedDeck | null>(null);
   const [deckToDelete, setDeckToDelete] = useState<SavedDeck | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const maxPages = Math.ceil(savedDecksInState.length / SAVED_DECKS_PER_PAGE);
+  const [state, dispatch] = useReducer(reducer, { modalVisible: false, selectedDeck: null });
+  const [scrollPositionY, setScrollPositionY] = useState<number>(0);
+  const [firstItemHeight, setFirstItemHeight] = useState<number>(0);
+  const [lastItemHeight, setLastItemHeight] = useState<number>(0);
+  const scrollUpArrowOpacity = useSharedValue(0);
+  const scrollDownArrowOpacity = useSharedValue(0);
+  const [savedDeckContainerHeight, setSavedDeckContainerHeight] = useState<number>(0);
+  const [savedDeckContentHeight, setSavedDeckContentHeight] = useState<number>(0);
+
+  const scrollUpArrowAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: scrollUpArrowOpacity.value,
+    };
+  });
+
+  const scrollDownArrowAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: scrollDownArrowOpacity.value,
+    };
+  });
 
   const handleClearDecks = () => {
     AsyncStorage.removeItem("saved-decks");
     clearDecks();
   };
 
-  const handleNextPage = () => {
-    if (currentPage < maxPages) {
-      setCurrentPage((prev) => prev + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
-    }
-  };
-
   const handleDeckListModal = (deck: SavedDeck) => {
-    setSelectedDeck(deck);
-    setDeckListModalIsVisible((prev) => !prev);
+    dispatch({ type: "OPEN_MODAL", payload: deck });
   };
 
   const handleDeleteDeck = (deck: SavedDeck) => {
@@ -69,80 +93,131 @@ const SavedDecks: React.FC<SavedDecksProps> = ({
     updateDeckInStorage(deck, newDeckName);
   };
 
+  const flatListRenderItem = ({ item, index }: { item: SavedDeck; index: number }) => {
+    return (
+      <View
+        style={styles.deckContainer}
+        onLayout={(event) => {
+          const { height } = event.nativeEvent.layout; // Get the height of the item
+          if (index === 0) {
+            setFirstItemHeight(Math.trunc(height)); // Set height of the first item
+          }
+
+          if (index === savedDecksInState.length - 1) {
+            setLastItemHeight(Math.trunc(height)); // Set height of the last item
+          }
+        }}
+      >
+        <View style={{ width: "100%", paddingHorizontal: 1, overflow: "hidden" }}>
+          <BlurredBackground>
+            <View style={styles.deck}>
+              <Pressable
+                onPress={() => {
+                  handleDeckListModal(item);
+                }}
+              >
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Text style={[globalStyles.text, styles.deckText]}>{item.deckName}</Text>
+                  <View style={{ marginHorizontal: 4 }}>
+                    <TabsIcon
+                      source={require("../../assets/tab-icons/cards-icon.svg")}
+                      focused={true}
+                    />
+                  </View>
+                  <Text style={[globalStyles.text, styles.deckText]}>{item.cards.length}</Text>
+                </View>
+              </Pressable>
+            </View>
+
+            <FontAwesome6
+              name="trash-alt"
+              size={24}
+              color={defaultColors.red}
+              onPress={() => {
+                setDeckToDelete(item);
+              }}
+            />
+
+            <ConfirmationModal
+              isVisible={deckToDelete != null}
+              text={`Are you sure you want to delete ${deckToDelete?.deckName}?`}
+              onConfirm={() => {
+                if (deckToDelete) {
+                  handleDeleteDeck(deckToDelete);
+                  setDeckToDelete(null);
+                }
+              }}
+              onCancel={() => {
+                setDeckToDelete(null);
+              }}
+              backgroundColor="rgba(0,0,0,0.1)"
+            />
+          </BlurredBackground>
+        </View>
+      </View>
+    );
+  };
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const { contentOffset } = event.nativeEvent;
+    setScrollPositionY(contentOffset.y);
+  };
+
+  const updateChevronOpacity = useCallback(() => {
+    if (scrollPositionY > firstItemHeight / 2) {
+      scrollUpArrowOpacity.value = withTiming(1, { duration: 200 });
+    } else {
+      scrollUpArrowOpacity.value = withTiming(0, { duration: 200 });
+    }
+
+    if (scrollPositionY + savedDeckContainerHeight < savedDeckContentHeight - lastItemHeight / 2) {
+      scrollDownArrowOpacity.value = withTiming(1, { duration: 200 });
+    } else {
+      scrollDownArrowOpacity.value = withTiming(0, { duration: 200 });
+    }
+  }, [
+    scrollPositionY,
+    firstItemHeight,
+    lastItemHeight,
+    savedDeckContainerHeight,
+    savedDeckContentHeight,
+    scrollUpArrowOpacity,
+    scrollDownArrowOpacity,
+  ]);
+
+  useEffect(() => {
+    updateChevronOpacity();
+  }, [savedDecksInState, updateChevronOpacity]);
+
   const deckContent =
     savedDecksInState.length > 0 ? (
-      savedDecksInState.map((deck, index) => {
-        if (
-          index < currentPage * SAVED_DECKS_PER_PAGE &&
-          index >= (currentPage - 1) * SAVED_DECKS_PER_PAGE
-        ) {
-          return (
-            <View key={Math.random()} style={styles.deckContainer}>
-              <BlurredBackground>
-                <SavedDeckModal
-                  modalVisible={deckListModalIsVisible}
-                  setVisible={() => setDeckListModalIsVisible(false)}
-                  deck={selectedDeck}
-                  updateDeck={handleUpdateDeck}
-                />
-                <View style={styles.deck}>
-                  <Pressable
-                    onPress={() => {
-                      onDeckSelectedForPlay && onDeckSelectedForPlay(deck);
-                    }}
-                  >
-                    <Text style={[globalStyles.text, styles.deckText]}>
-                      {deck.deckName}
-                      {
-                        <View>
-                          <TabsIcon
-                            source={require("../../assets/tab-icons/cards-icon.svg")}
-                            focused={true}
-                            top={8}
-                          />
-                        </View>
-                      }
-                      {deck.cards.length}
-                    </Text>
-                  </Pressable>
-                </View>
-                {canDeleteDeck ? (
-                  <FontAwesome6
-                    name="trash-alt"
-                    size={24}
-                    color={defaultColors.red}
-                    onPress={() => {
-                      setDeckToDelete(deck);
-                    }}
-                  />
-                ) : (
-                  <CustomButton
-                    type="positive"
-                    text="View"
-                    onPress={() => {
-                      handleDeckListModal(deck);
-                    }}
-                  />
-                )}
-                <ConfirmationModal
-                  isVisible={deckToDelete != null}
-                  text={`Are you sure you want to delete ${deckToDelete?.deckName}?`}
-                  onConfirm={() => {
-                    if (deckToDelete) {
-                      handleDeleteDeck(deckToDelete);
-                      setDeckToDelete(null);
-                    }
-                  }}
-                  onCancel={() => {
-                    setDeckToDelete(null);
-                  }}
-                />
-              </BlurredBackground>
-            </View>
-          );
-        }
-        return null;
-      })
+      <>
+        <Animated.View style={scrollUpArrowAnimatedStyle}>
+          <FontAwesome
+            style={styles.scrollChevronIcon}
+            name="chevron-up"
+            size={14}
+            color={"white"}
+          />
+        </Animated.View>
+
+        <FlatList
+          data={savedDecksInState}
+          renderItem={flatListRenderItem}
+          onScroll={onScroll}
+          scrollEventThrottle={16}
+          onContentSizeChange={(width, height) => setSavedDeckContentHeight(height)}
+        />
+
+        <Animated.View style={scrollDownArrowAnimatedStyle}>
+          <FontAwesome
+            style={styles.scrollChevronIcon}
+            name="chevron-down"
+            size={14}
+            color={"white"}
+          />
+        </Animated.View>
+      </>
     ) : (
       // <Link href="/(tabs)/deckbuilder">
       <Pressable onPress={() => router.push("/(tabs)/deckbuilder")}>
@@ -151,26 +226,25 @@ const SavedDecks: React.FC<SavedDecksProps> = ({
       // </Link>
     );
 
-  const paginationContent = (
-    <View style={styles.paginationContainer}>
-      <AnimatedIcon onPress={handlePreviousPage} visible={currentPage > 1}>
-        <FontAwesome6 name="chevron-left" size={24} color={defaultColors.gold} />
-      </AnimatedIcon>
-      <Text style={[globalStyles.text, styles.emptyDeckText]}>
-        {currentPage} / {maxPages}
-      </Text>
-      <AnimatedIcon onPress={handleNextPage} visible={currentPage < maxPages}>
-        <FontAwesome6 name="chevron-right" size={24} color={defaultColors.gold} />
-      </AnimatedIcon>
-    </View>
-  );
-
   return (
     <View style={styles.container}>
-      <View style={styles.allDecksContainer}>{deckContent}</View>
-      {savedDecksInState.length > SAVED_DECKS_PER_PAGE && <View>{paginationContent}</View>}
+      {state.selectedDeck && (
+        <SavedDeckModal
+          modalVisible={state.modalVisible}
+          setVisible={() => dispatch({ type: "CLOSE_MODAL" })}
+          deck={state.selectedDeck}
+          updateDeck={handleUpdateDeck}
+          selectDeckForPlay={onDeckSelectedForPlay}
+        />
+      )}
+      <View
+        style={styles.allDecksContainer}
+        onLayout={(e) => setSavedDeckContainerHeight(e.nativeEvent.layout.height)}
+      >
+        {deckContent}
+      </View>
       <Spacer height={50} />
-      {savedDecksInState.length > 0 && canClearDeckList && (
+      {savedDecksInState.length > 0 && true && (
         <CustomButton
           text="Clear decks"
           type="negative"
@@ -191,11 +265,15 @@ const SavedDecks: React.FC<SavedDecksProps> = ({
 };
 
 const styles = StyleSheet.create({
+  scrollChevronIcon: {
+    transform: [{ scaleX: 5 }],
+    marginVertical: 10,
+  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-end",
-    marginBottom: 70,
+    marginBottom: 60,
   },
   allDecksContainer: {
     width: "96%",
@@ -209,11 +287,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    paddingHorizontal: 2,
   },
   deck: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    marginHorizontal: 20,
   },
   deckText: {
     color: defaultColors.gold,
