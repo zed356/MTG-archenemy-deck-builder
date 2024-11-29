acd3f1d4f91fc0f2a04ecb8daf8f2a6c7d0ab513
z <zilvistam@gmail.com>
2024-11-29 20:49:08 +0000

    feat: arrow to scroll back to top of screen; partial fix to scroll in relation to header height

--- components/navigation/DeckBuilder.tsx
+++ components/navigation/DeckBuilder.tsx
@@ -2,9 +2,24 @@ import { defaultColors } from "@/constants/Colors";
 import { globalStyles } from "@/constants/styles";
 import { MINIMUM_CARDS_IN_NEW_DECK } from "@/constants/values";
 import { useCardStore, useNewDeckStore } from "@/store/store";
+import { FontAwesome } from "@expo/vector-icons";
 import { ScryfallCard } from "@scryfall/api-types";
-import { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useRef, useState } from "react";
+import {
+  FlatList,
+  LayoutChangeEvent,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 import Card from "../card/Card";
 import Filter from "../card/filter/Filter";
 import NewDeck from "../decks/NewDeck";
@@ -15,34 +30,55 @@ const DeckBuilder: React.FC = () => {
   const { cardsInNewDeck, addCardToNewDeck, removeCardFromNewDeck } = useNewDeckStore();
   const { cardsInStore, error } = useCardStore();
   const [displayedCards, setDisplayedCards] = useState<ScryfallCard.Scheme[]>([]);
+  const [currentScrollPositionY, setCurrentScrollPositionY] = useState(0);
+  const [upArrowToTopOfScreenWidth, setUpArrowToTopOfScreenWidth] = useState(0);
+  const [initialNewDeckContainerHeight, setInitialNewDeckContainerHeight] = useState(0);
+  const cardFlatListRef = useRef<FlatList<ScryfallCard.Scheme>>(null);
+  const arrowToTopOfScreenOpacity = useSharedValue(1);
+
+  const arrowToTopOfScreenAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: arrowToTopOfScreenOpacity.value,
+    };
+  });
 
   if (error) {
     return <ErrorModal errorMessage={error} />;
   }
 
-  const styles = StyleSheet.create({
-    text: {
-      color: cardsInNewDeck.length < MINIMUM_CARDS_IN_NEW_DECK ? defaultColors.red : "#42b883",
-      fontSize: 18,
-      margin: 0,
-      paddingBottom: 5,
-    },
-    scrollContainer: { flex: 1, marginVertical: 30 },
-    container: {
-      flex: 1,
-      justifyContent: "space-evenly",
-      marginTop: 10,
-    },
-    cardWrapper: {
-      flex: 1,
-      marginVertical: 10,
-      marginHorizontal: 5, // Add some horizontal margin for spacing
-      alignItems: "center", // Center the columns
-    },
-    columnWrapper: {
-      justifyContent: "space-between", // Optional: to evenly space columns
-    },
-  });
+  const handleOnScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    setCurrentScrollPositionY(e.nativeEvent.contentOffset.y);
+  };
+
+  const handleScrollToTopOfScreen = () => {
+    arrowToTopOfScreenOpacity.value = withRepeat(withTiming(0.5, { duration: 100 }), 2, true);
+    if (cardFlatListRef.current) {
+      cardFlatListRef.current.scrollToOffset({ offset: 0, animated: true });
+    }
+  };
+
+  const handleNewDeckLayout = (layout: LayoutChangeEvent) => {
+    const { height } = layout.nativeEvent.layout;
+    // Set the initial height of the new deck container for use when it is empty and need to deduct its height from the scroll position
+    if (initialNewDeckContainerHeight === 0) {
+      setInitialNewDeckContainerHeight(height);
+    }
+
+    if (cardFlatListRef.current) {
+      if (height === 0) {
+        // If the new deck container is empty, deduct its 'first' height after appearing, from the scroll position
+        cardFlatListRef.current.scrollToOffset({
+          offset: currentScrollPositionY - initialNewDeckContainerHeight,
+          animated: true,
+        });
+      } else {
+        cardFlatListRef.current.scrollToOffset({
+          offset: currentScrollPositionY + height,
+          animated: true,
+        });
+      }
+    }
+  };
 
   const renderItem = ({ item }: { item: ScryfallCard.Scheme }) => (
     <View style={styles.cardWrapper}>
@@ -61,10 +97,41 @@ const DeckBuilder: React.FC = () => {
 
   const headerComponent = (
     <>
-      <NewDeck />
+      <NewDeck onLayoutChange={handleNewDeckLayout} />
       <Filter cards={cardsInStore} setFilteredCards={setDisplayedCards} />
     </>
   );
+
+  const styles = StyleSheet.create({
+    text: {
+      color: cardsInNewDeck.length < MINIMUM_CARDS_IN_NEW_DECK ? defaultColors.red : "#42b883",
+      fontSize: 18,
+      margin: 0,
+      paddingBottom: 5,
+    },
+    scrollContainer: { flex: 1, marginVertical: 30, position: "relative" },
+    container: {
+      flex: 1,
+      justifyContent: "space-evenly",
+      marginTop: 10,
+    },
+    cardWrapper: {
+      flex: 1,
+      marginVertical: 10,
+      marginHorizontal: 5, // Add some horizontal margin for spacing
+      alignItems: "center", // Center the columns
+    },
+    columnWrapper: {
+      justifyContent: "space-between", // Optional: to evenly space columns
+    },
+    arrowUpToTopOfScreenContainer: {
+      position: "absolute",
+      color: defaultColors.grey,
+      transform: [{ scaleY: 1.2 }, { translateX: -upArrowToTopOfScreenWidth / 2 }],
+      top: "90%",
+      left: "50%",
+    },
+  });
 
   return (
     <View style={styles.scrollContainer}>
@@ -74,6 +141,7 @@ const DeckBuilder: React.FC = () => {
         </Text>
       </PulseWrapper>
       <FlatList
+        ref={cardFlatListRef}
         ListHeaderComponent={headerComponent}
         data={displayedCards}
         renderItem={renderItem}
@@ -84,7 +152,22 @@ const DeckBuilder: React.FC = () => {
         maxToRenderPerBatch={6}
         contentContainerStyle={{ flexGrow: 1 }}
         keyboardShouldPersistTaps="handled"
+        onScroll={handleOnScroll}
+        scrollEventThrottle={16}
       />
+      {currentScrollPositionY > 500 && (
+        <Animated.View
+          style={[arrowToTopOfScreenAnimatedStyle, styles.arrowUpToTopOfScreenContainer]}
+          onLayout={(event) => setUpArrowToTopOfScreenWidth(event.nativeEvent.layout.width)}
+        >
+          <FontAwesome
+            name="arrow-up"
+            size={24}
+            color={defaultColors.grey}
+            onPress={handleScrollToTopOfScreen}
+          />
+        </Animated.View>
+      )}
     </View>
   );
 };
